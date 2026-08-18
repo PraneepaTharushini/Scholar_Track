bc34b12a98035b9b9fb13daee3d267597fbd2b49
Y.M. Praneepa Tharushini <22fis0564@ms.sab.ac.lk>
2026-08-18 22:09:32 +0530

    update run.py

--- backend/run.py
+++ backend/run.py
@@ -1,13 +1,20 @@
 import os
-
 from dotenv import load_dotenv
-
 from app import create_app
-
+from flask import send_from_directory
 
 load_dotenv()
 app = create_app()
 
+# This tells Python how to show your React website
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve_react(path):
+    static_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
+    if path != "" and os.path.exists(os.path.join(static_folder, path)):
+        return send_from_directory(static_folder, path)
+    else:
+        return send_from_directory(static_folder, 'index.html')
 
 if __name__ == "__main__":
     host = os.getenv("FLASK_HOST", "127.0.0.1")
